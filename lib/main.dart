e0d77bfab0e8975008c93ad23845c2bbca46ef0c
AMOREM <amorem@amorem.com>
2019-05-21 17:14:46 +0200

    Container + Row/Column tuto

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              height: double.infinity,
+              width: 100.0,
+              color: Colors.redAccent,
+              child: Text('Container 1'),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.yellowAccent,
+                  child: Text('Container 3'),
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.greenAccent,
+                  child: Text('Container 4'),
+                )
+              ],
+            ),
+            Container(
+              height: double.infinity,
+              width: 100.0,
+              color: Colors.blueAccent,
+              child: Text('Container 2'),
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
